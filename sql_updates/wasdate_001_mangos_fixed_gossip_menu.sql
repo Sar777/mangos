e488d997466e77d1cd4c3eeff36c21a55c64c63e
Sar <orionic.gm@gmail.com>
2011-12-04 17:18:43 +0300

    Fixed gossip menu, iccecrown

---
+++ sql_updates/wasdate_001_mangos_fixed_gossip_menu.sql
@@ -0,0 +1,4 @@
+-- chemp icecrown
+update `gossip_menu_option` set `option_id`='19' where `menu_id`='10318' and `id`='2';
+
+update `gossip_menu_option` set `option_id`='19' where `menu_id`='33239' and `id`='2';
