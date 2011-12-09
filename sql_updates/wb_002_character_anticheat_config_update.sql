cbff8e0e81822f4bf72a746b4d2cf46e6deb9fe6
Sar <orionic.gm@gmail.com>
2011-12-09 13:06:06 +0300

    Rename sql file last commit

--- sql_updates/wb_002_character_anticheat_config_update.sql
+++
@@ -1,5 +0,0 @@
--- Arena The Ring of Valor
-UPDATE `anticheat_config` SET `disabledzones`='4406' WHERE (`checktype`='109');
-
--- kick player
-UPDATE `anticheat_config` SET `action2`='4' WHERE (`checktype`='701');
