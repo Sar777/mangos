4f2f2a669a85ad1ee1abc871d3e8a3f2529c3a70
Sar <orionic.gm@gmail.com>
2011-12-14 16:46:00 +0300

    Added new table in characters for last commit

---
+++ sql_updates/wb_028_characters_temp_ban_wpe.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `temp_ban_wpe`
+-- ----------------------------
+DROP TABLE IF EXISTS `temp_ban_wpe`;
+CREATE TABLE `temp_ban_wpe` (
+  `guid` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
